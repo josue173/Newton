9e69f6152739bca93cb1c2b84bf93bbc3cc84e6f
josue173 <josue.perez04@outlook.com>
2024-03-03 18:15:40 -0600

    Indicando valor inicial

--- Newton.java
+++ Newton.java
@@ -14,7 +14,7 @@ public class Newton {
 
     public static void main(String[] args) {
         System.out.println("Ingrese el valor inicial: ");
-        double inicio = leer.nextDouble();
+        double inicio = leer.nextDouble(); // 3
         iterar(inicio);
         mostrar();
     }
@@ -41,12 +41,12 @@ public class Newton {
     }
     
     public static double funcion(double x){
-        return  Math.pow(x, 3) - Math.pow(Math.E, 2) + (6*x) - 24; // x^3 - e^2 + 6x - 24
+        return  3*Math.pow(x,2) - Math.pow(Math.E, x) + 7; // 3x^2 - e^x + 7
         
     }
     
     public static double derivada(double x){
-        return Math.pow(3*x, 2) - 6; // 3x^2 - 6
+        return 6*x - Math.pow(Math.E, x); // 6x−e^x
     }
     
     public static void mostrar() {
